852da3bac2ea72f0ab8f063382c237c7262c0e87
Replit user <>
2023-07-06 05:35:12 +0000

    chore: Send response

--- routes/fileanalyse.js
+++ routes/fileanalyse.js
@@ -5,8 +5,13 @@ const router = Router();
 const upload = multer({ dest: 'uploads/' });
 
 router.post('/', upload.single('upfile'), (req, res, next) => {
-  
-  console.log(JSON.stringify(req.file));
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size
+  });
 });
 
 export default router;
