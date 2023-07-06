1873b36d65ebf290a46f5f54c82f85d6035b1062
Replit user <>
2023-07-06 02:43:07 +0000

    refactor: To ES6 modules

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+import cors from 'cors';
+import express from 'express';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+import path, { dirname } from 'path';
 
-var app = express();
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+const server = app.listen(port, () => {
+  console.log('Your app is listening on port ' + port);
 });
